9ef75bf684252dce3e45e94b726292ecee1d2f8d
michaelspiss <michaelspiss@users.noreply.github.com>
2018-06-13 08:51:26 +0200

    Make starttls return the ImapConnection for further commands
    STARTTLS requires receiving and sending messages from and to the server. This is by far perfect and might even cause problems for people using it, but it must suffice for now.

--- lib/src/imap_client.dart
+++ lib/src/imap_client.dart
@@ -173,9 +173,12 @@ class ImapClient {
   /// Deprecated by RFC 8314, use a secure connection if possible.
   /// TLS negotiation is not part of this package. It begins *after* command
   /// completion, no further commands must be issued while the negotiation is
-  /// not complete.
+  /// not complete. Returns the [ImapConnection] so a communication with the
+  /// server is possible to implement.
+  /// Again, this method should not be used anymore!
   @deprecated
-  Future<List<String>> starttls() {
-    return sendCommand('STARTTLS');
+  ImapConnection starttls() {
+    sendCommand('STARTTLS');
+    return _connection;
   }
 }
