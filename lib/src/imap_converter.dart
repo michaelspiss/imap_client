36ad719ed3f74b9390d173c9bdf330a2a3b5edab
michaelspiss <michaelspiss.dev@mailbox.org>
2018-07-05 22:39:51 +0200

    Correct wording

--- lib/src/imap_converter.dart
+++ lib/src/imap_converter.dart
@@ -1,6 +1,6 @@
 part of ImapClient;
 
-/// A class containing static methods to convert between imap and dart list/map
+/// Contains static methods to convert between imap and dart lists/maps
 class ImapConverter {
 
   /// Converts a regular list to a parenthesized imap list
