397e5e3da1b681c1b417147268a8ec7c3660cbc4
michaelspiss <michaelspiss.dev@mailbox.org>
2019-02-05 20:56:54 +0100

    Add description to _BufferAwaiter

--- lib/src/buffer_awaiter.dart
+++ lib/src/buffer_awaiter.dart
@@ -1,6 +1,8 @@
 part of imap_client;
 
 /// Gets instantiated when [ImapBuffer] did not receive the requested char yet
+///
+/// See implementation in [ImapBuffer] _getCharCode() method
 class _BufferAwaiter {
   final Completer completer;
   final int awaitedPosition;
