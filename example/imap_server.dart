da9ad938a4a71062c535f2c68bdd9bb252ca75b3
michaelspiss <michaelspiss.dev@mailbox.org>
2019-03-09 23:27:40 +0100

    Remove unneeded example file

--- example/imap_server.dart
+++
@@ -1,7 +0,0 @@
-import 'package:imap_client/imap_client.dart';
-
-main() async {
-  printImapClientDebugLog();
-  ImapClient client = new ImapClient();
-  await client.connect("imap.web.de", 993, true);
-}
