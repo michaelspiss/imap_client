aaffbd8d747790bc50878983625a076cb8b04681
Michael Spiss <michaelspiss.dev@mailbox.org>
2019-03-10 01:02:33 +0100

    Make example more meaningful

--- example/fetch.dart
+++ example/fetch.dart
@@ -1,12 +1,20 @@
 import 'package:imap_client/imap_client.dart';
 
 main() async {
+  // print log, helpful for debugging
   printImapClientDebugLog();
+  
   ImapClient client = new ImapClient();
+  // connect
   await client.connect("imap.gmail.com", 993, true);
+  // authenticate
   await client.authenticate(new ImapPlainAuth("user@gmail.com", "password"));
+  // get folder
   ImapFolder inbox = await client.getFolder("inbox");
+  // get "BODY" for message 1
   print(await inbox.fetch(["BODY"], messageIds: [1]));
+  // get "BODYSTRUCTURE" for message 1
   print(await inbox.fetch(["BODYSTRUCTURE"], messageIds: [1]));
+  // close connection
   await client.logout();
 }
